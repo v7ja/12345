cd85852fc51f1f5cc2de0d43cf14cf94edaefca9
v7ja <142932805+v7ja@users.noreply.github.com>
2024-03-29 23:25:22 +0300

    Update tool.py

--- tool.py
+++ tool.py
@@ -125,9 +125,9 @@ def checker(username,client):
 # for generate username
 def usernameG():
   k = ''.join(choice('qwertyuiopasdfghjklzxcvbnm') for i in range(1))
-  a = ''.join(choice('qwertyuiopasdfghjklzxcvbnm') for i in range(1))
+  a = ''.join(choice('qwertyuiopasdfghjklzxcvbnm1234567890') for i in range(1))
   n = ''.join(choice('1234567890') for i in range(1))
-  return k+a+n+'Bot'
+  return k+a+a+'_'+k
 # start checking
 def start(client,username):
   try:ok = fragment(username)
